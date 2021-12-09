5c40e57496dbc668960235db641755691eee9103
Dzeganovsky <idz@efg.com.ua>
2021-12-09 20:11:31 +0200

    C

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,39 @@ const less = require("gulp-less");
 const concat = require("gulp-concat");
 const browserSync  = require('browser-sync').create();
 const reload = browserSync.reload;
+const nunjucks = require('gulp-nunjucks');
+const avif = require('gulp-avif');
+const webp = require('gulp-webp');
+
+function compileNjs() {
+  return gulp.src('templates/*.html')
+          .pipe(nunjucks.compile())
+          .pipe(gulp.dest('./'));
+}
+
+function iWebp() {
+  return gulp.src('img/dummy/*.png')
+          .pipe(webp())
+          .pipe(gulp.dest('./img/dummy'));
+}
+
+function iAvif() {
+  return gulp.src('img/dummy/*.png')
+          .pipe(avif())
+          .pipe(gulp.dest('./img/dummy'));
+}
 
 function compileLess() {
-  return gulp.src("./less/**/*.less").pipe(less()).pipe(concat("style.css")).pipe(gulp.dest("./dist"));
+  return gulp.src("./less/**/*.less")
+          .pipe(less())
+          .pipe(concat("style.css"))
+          .pipe(gulp.dest("./dist"));
 }
 
 function watcher(done) {
   watch(["./less/**/*.less"], parallel(compileLess));
+  watch(["./templates/*.html"], parallel(compileNjs));
+  watch(["./img/dummy/*.png"], parallel(iWebp, iAvif));
 
   done();
 }
@@ -23,8 +49,8 @@ function dev(done) {
     },
   });
 
-  watch(["./**/*.html", "./**/*.css"]).on("change", reload);
+  watch(["./*.html", "./**/*.css"]).on("change", reload);
   done();
 };
 
-exports.serve = series(compileLess, dev, watcher);
+exports.serve = series(parallel(compileNjs, compileLess, iWebp, iAvif), dev, watcher);
